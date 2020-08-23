73088a2b74403d38fb97286b85acdf7de687f421
Maurya Patel <mauryapatel@Chaitrangis-MacBook-Pro.local>
2020-08-23 17:49:11 -0400

    changed the routing method from app to router

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,60 +1,23 @@
-// file where the routing to apis will be done
-// import required modules 
-const fs = require("fs");
-const noteData = require("../db/db.json");
+const router = require("express").Router();
+const noteDB = require('../db/notesDB');
 
-module.exports = function(app) {
-    function updateDB(notes) {
-        // Converts the json array into a string
-        notes = JSON.stringify(notes);
-        console.log(notes);
-        // writes String back to db.json
-        fs.writeFileSync("/db/db.json", notes,function(err){
-            if(err) throw err;
-        });
-    }
+router.get("/notes",function(req,res){
+    noteDB.getNotes()
+    .then(notes => res.json(notes))
+    .catch(err => res.status(500).json(err));
+});
 
-    // api routes
-    app.get("/api/notes", function(req, res){
-        res.json(noteData);
-    });
+router.post("/notes", function(req,res){
+    console.log(req.body);
+    noteDB.addNote(req.body)
+    .then(note => res.json(note))
+    .catch(err => res.status(500).json(err));
+});
 
-    app.post("/api/notes", function(req, res){
-        // set a unique id to each note create by the user
-        if(noteData.length === 0){
-            req.body.id = 0;
-        }
-        else {
-            req.body.id = JSON.stringify(JSON.parse(noteData[noteData.length - 1].id) + 1);
-        }
+router.delete("/notes/:id", function(req,res){
+    noteDB.removeNote(req.params.id)
+    .then(() => res.json({ok: true}))
+    .catch(err => res.status(500).json(err));
+});
 
-        console.log("ID: " + req.body.id);
-
-        // populate the new array by pushing things into it
-        noteData.push(req.body);
-
-        // update notes data to database
-        updateDB(noteData);
-        // logging to see everything got pushed
-        console.log(updateDB);
-    });
-
-    // a method to delete each note individually with the help of a unique id
-    app.delete("/api/notes/:id", function(req,res){
-        // get id and convert them into string
-        let id = req.params.id.toString();
-        console.log("id is:" + id);
-
-        // loop through the array to check for matching id 
-        for(i=0; i < noteData.length; i++){
-            if(noteData[i].id == id) {
-                res.send(noteData[i]);
-                // removes the deleted note from the array
-                noteData.splice(i,1);
-                break;
-            }
-        }
-        // update notes into database
-        updateDB(noteData);
-    });
-}
+module.exports = router
